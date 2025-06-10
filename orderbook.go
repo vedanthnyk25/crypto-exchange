c764cf81cbfcd265d991fe21ff0a17162e23a4af
vedanthnyk25 <vedanthnayak25@gmail.com>
2025-06-10 14:59:40 +0530

    Refactor order management by introducing Orders and Limits types

--- orderbook.go
+++ orderbook.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"slices"
+	"sort"
+	"time"
+)
 
 type Match struct {
 	Ask        *Order
@@ -14,6 +18,12 @@ type Order struct {
 	Bid       bool
 	Timestamp int64
 }
+
+type Orders []*Order
+
+func (o Orders) Len() int           { return len(o) }
+func (o Orders) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
+func (o Orders) Less(i, j int) bool { return o[i].Timestamp < o[j].Timestamp }
 
 func NewOrder(bid bool, size float64) *Order {
 	return &Order{
@@ -32,19 +42,35 @@ func (l *Limit) AddOrder(o *Order) {
 func (l *Limit) DeleteOrder(o *Order) {
 	for i, order := range l.Orders {
 		if order == o {
-			l.Orders = append(l.Orders[:i], l.Orders[i+1:]...)
+			l.Orders = slices.Delete(l.Orders, i, 1)
 			break
 		}
 	}
+
 	o.Limit = nil
 	l.TotalVolume -= o.Size
+
+	sort.Sort(l.Orders)
 }
 
 type Limit struct {
 	Price       float64
-	Orders      []*Order
+	Orders      Orders
 	TotalVolume float64
 }
+
+type Limits []*Limit
+type ByBestAsk struct{ Limits }
+
+func (a ByBestAsk) Len() int           { return len(a.Limits) }
+func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
+func (a ByBestAsk) Less(i, j int) bool { return a.Limits[i].Price < a.Limits[j].Price }
+
+type ByBestBid struct{ Limits }
+
+func (b ByBestBid) Len() int           { return len(b.Limits) }
+func (b ByBestBid) Swap(i, j int)      { b.Limits[i], b.Limits[j] = b.Limits[j], b.Limits[i] }
+func (b ByBestBid) Less(i, j int) bool { return b.Limits[i].Price > b.Limits[j].Price }
 
 func NewLimit(price float64) *Limit {
 	return &Limit{
@@ -69,10 +95,10 @@ func NewOrderbook() *Orderbook {
 	}
 }
 
-func (ob *Orderbook) PlaceOrder(price float64, o *Order) []Match{
-  //1. Matching logic
+func (ob *Orderbook) PlaceOrder(price float64, o *Order) []Match {
+	//1. Matching logic
 
-	if o.Size>0{
+	if o.Size > 0 {
 		ob.add(price, o)
 	}
 
@@ -80,26 +106,25 @@ func (ob *Orderbook) PlaceOrder(price float64, o *Order) []Match{
 }
 
 func (ob *Orderbook) add(price float64, o *Order) {
-    var limit *Limit
-		
-		if o.Bid{
-      limit= ob.BidLimits[price]
-		}else{
-			limit= ob.AskLimits[price]
-		}
+	var limit *Limit
 
-		if limit==nil {
+	if o.Bid {
+		limit = ob.BidLimits[price]
+	} else {
+		limit = ob.AskLimits[price]
+	}
 
-			limit= NewLimit(price)
-			limit.AddOrder(o)
+	if limit == nil {
 
-			if o.Bid{
-				ob.Bids= append(ob.Asks, *limit)
-				ob.BidLimits[price]=limit
-			}else{
-				ob.Asks= append(ob.Bids, *limit)
-				ob.AskLimits[price]=limit
-			}
-		}
+		limit = NewLimit(price)
+		limit.AddOrder(o)
+
+		if o.Bid {
+    ob.Bids = append(ob.Bids, *limit)
+    ob.BidLimits[price] = limit
+} else {
+    ob.Asks = append(ob.Asks, *limit)
+    ob.AskLimits[price] = limit
 }
-
+	}
+}
